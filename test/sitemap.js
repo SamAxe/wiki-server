019ebf3b41398076414cf29bbd865e69c913c977
decaffeinate <693896-SamAxe@users.noreply.gitlab.com>
2024-03-01 19:20:53 +0000

    decaffeinate: Convert sitemap.coffee to JS

--- test/sitemap.js
+++ test/sitemap.js
@@ -1,110 +1,121 @@
-request = require 'supertest'
-fs = require 'fs'
-server = require '..'
-path = require 'path'
-random = require '../lib/random_id'
-testid = random()
-argv = require('../lib/defaultargs.coffee')({data: path.join('/tmp', 'sfwtests', testid), port: 55556, security_legacy: true})
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
+ */
+let request = require('supertest');
+const fs = require('fs');
+const server = require('..');
+const path = require('path');
+const random = require('../lib/random_id');
+const testid = random();
+const argv = require('../lib/defaultargs.coffee')({data: path.join('/tmp', 'sfwtests', testid), port: 55556, security_legacy: true});
 
-describe 'sitemap', ->
-  app = {}
-  runningServer = null
-  beforeEach((done) ->
-    app = server(argv)
-    app.once("owner-set", ->
-      runningServer = app.listen app.startOpts.port, app.startOpts.host, done
-    ))
-  afterEach () ->
-    runningServer.close()
-
-
-  request = request('http://localhost:55556')
-  fs.mkdirSync path.join('/tmp', 'sfwtests', testid, 'pages'), {recursive: true}
-
-  # location of the sitemap
-  sitemapLoc = path.join('/tmp', 'sfwtests', testid, 'status', 'sitemap.json')
-
-  it 'new site should have an empty sitemap', () ->
-    request
-    .get('/system/sitemap.json')
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .then (res) ->
-      res.body.should.be.empty
-    , (err) ->
-      throw err
+describe('sitemap', function() {
+  let app = {};
+  let runningServer = null;
+  beforeEach(function(done) {
+    app = server(argv);
+    return app.once("owner-set", () => runningServer = app.listen(app.startOpts.port, app.startOpts.host, done));});
+  afterEach(() => runningServer.close());
 
 
-  it 'creating a page should add it to the sitemap', () ->
-    body = JSON.stringify({
-      type: 'create'
+  request = request('http://localhost:55556');
+  fs.mkdirSync(path.join('/tmp', 'sfwtests', testid, 'pages'), {recursive: true});
+
+  // location of the sitemap
+  const sitemapLoc = path.join('/tmp', 'sfwtests', testid, 'status', 'sitemap.json');
+
+  it('new site should have an empty sitemap', () => request
+  .get('/system/sitemap.json')
+  .expect(200)
+  .expect('Content-Type', /json/)
+  .then(res => res.body.should.be.empty
+  , function(err) {
+    throw err;
+  }));
+
+
+  it('creating a page should add it to the sitemap', function() {
+    const body = JSON.stringify({
+      type: 'create',
       item: {
-        title: "Asdf Test Page"
+        title: "Asdf Test Page",
         story: [
-          {id: "a1", type: "paragraph", text: "this is the first paragraph"}
-          {id: "a2", type: "paragraph", text: "this is the second paragraph"}
-          {id: "a3", type: "paragraph", text: "this is the third paragraph"}
+          {id: "a1", type: "paragraph", text: "this is the first paragraph"},
+          {id: "a2", type: "paragraph", text: "this is the second paragraph"},
+          {id: "a3", type: "paragraph", text: "this is the third paragraph"},
           {id: "a4", type: "paragraph", text: "this is the fourth paragraph"}
           ]
-      }
+      },
       date: 1234567890123
-      })
+      });
 
-    request
+    return request
       .put('/page/adsf-test-page/action')
       .send("action=" + body)
       .expect(200)
-      .then (res) ->
-        # sitemap update does not happen until after the put has returned, so wait for it to finish
-        app.sitemaphandler.once 'finished', ->
-          try
-            sitemap = JSON.parse(fs.readFileSync(sitemapLoc))
-          catch err
-            throw err
-          sitemap[0].slug.should.equal['adsf-test-page']
-          sitemap[0].synopsis.should.equal['this is the first paragraph']
-      , (err) ->
-        throw err
+      .then(res => // sitemap update does not happen until after the put has returned, so wait for it to finish
+    app.sitemaphandler.once('finished', function() {
+      let sitemap;
+      try {
+        sitemap = JSON.parse(fs.readFileSync(sitemapLoc));
+      } catch (err) {
+        throw err;
+      }
+      sitemap[0].slug.should.equal['adsf-test-page'];
+      return sitemap[0].synopsis.should.equal['this is the first paragraph'];
+  })
+      , function(err) {
+        throw err;
+    });
+  });
 
-  it 'synopsis should reflect edit to first paragraph', () ->
-    body = JSON.stringify({
-      type: 'edit'
-      item: {id: 'a1', type: 'paragraph', text: 'edited'}
+  it('synopsis should reflect edit to first paragraph', function() {
+    const body = JSON.stringify({
+      type: 'edit',
+      item: {id: 'a1', type: 'paragraph', text: 'edited'},
       id: 'a1'
-    })
+    });
 
-    request
+    return request
       .put('/page/adsf-test-page/action')
       .send("action=" + body)
       .expect(200)
-      .then (res) ->
-        app.sitemaphandler.once 'finished', ->
-          try
-            sitemap = JSON.parse(fs.readFileSync(sitemapLoc))
-          catch err
-            throw err
-          sitemap[0].slug.should.equal['adsf-test-page']
-          sitemap[0].synopsis.should.equal['edited']
-      , (err) ->
-        throw err
+      .then(res => app.sitemaphandler.once('finished', function() {
+      let sitemap;
+      try {
+        sitemap = JSON.parse(fs.readFileSync(sitemapLoc));
+      } catch (err) {
+        throw err;
+      }
+      sitemap[0].slug.should.equal['adsf-test-page'];
+      return sitemap[0].synopsis.should.equal['edited'];
+  })
+      , function(err) {
+        throw err;
+    });
+  });
 
-  it 'deleting a page should remove it from the sitemap', () ->
+  it('deleting a page should remove it from the sitemap', () => request
+    .delete('/adsf-test-page.json')
+    .send()
+    .expect(200)
+    .then(res => app.sitemaphandler.once('finished', function() {
+    let sitemap;
+    try {
+      sitemap = JSON.parse(fs.readFileSync(sitemapLoc));
+    } catch (error) {
+      throw err;
+    }
+    return sitemap.should.be.empty;
+  })
+    , function(err) {
+      throw err;
+  }));
 
-    request
-      .delete('/adsf-test-page.json')
-      .send()
-      .expect(200)
-      .then (res) ->
-        app.sitemaphandler.once 'finished', ->
-          try
-            sitemap = JSON.parse(fs.readFileSync(sitemapLoc))
-          catch error
-            throw err
-          sitemap.should.be.empty
-      , (err) ->
-        throw err
 
 
-
-  after( ->
-    app.close() if app.close)
+  return after( function() {
+    if (app.close) { return app.close(); }});
+});
