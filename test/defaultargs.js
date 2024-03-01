9b8a6411a261e9d6a28dee733aff2a0b5e4b1e90
decaffeinate <693896-SamAxe@users.noreply.gitlab.com>
2024-03-01 19:20:46 +0000

    decaffeinate: Convert defaultargs.coffee to JS

--- test/defaultargs.js
+++ test/defaultargs.js
@@ -1,10 +1,12 @@
-defaultargs = require '../lib/defaultargs'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
+ */
+const defaultargs = require('../lib/defaultargs');
 
-describe 'defaultargs', ->
-  describe '#defaultargs()', ->
-    it 'should not write over give args', ->
-      defaultargs({port: 1234}).port.should.equal(1234)
-    it 'should write non give args', ->
-      defaultargs().port.should.equal(3000)
-    it 'should modify dependant args', ->
-      defaultargs({data: '/tmp/asdf/'}).db.should.equal('/tmp/asdf/pages')	
+describe('defaultargs', () => describe('#defaultargs()', function() {
+  it('should not write over give args', () => defaultargs({port: 1234}).port.should.equal(1234));
+  it('should write non give args', () => defaultargs().port.should.equal(3000));
+  return it('should modify dependant args', () => defaultargs({data: '/tmp/asdf/'}).db.should.equal('/tmp/asdf/pages'));
+}));	
