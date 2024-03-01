ae152bc22bb99b7f3b1f80c0ba372ba2dc65df1b
decaffeinate <693896-SamAxe@users.noreply.gitlab.com>
2024-03-01 19:20:44 +0000

    decaffeinate: Convert random.coffee to JS

--- test/random.js
+++ test/random.js
@@ -1,8 +1,11 @@
-random = require '../lib/random_id'
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
+ */
+const random = require('../lib/random_id');
 
-describe 'random', ->
-  describe '#random_id', ->
-    it 'should not be the same twice', ->
-      random().should.not.equal random()
-    it 'should be 16 digits', ->
-      random().length.should.equal 16
+describe('random', () => describe('#random_id', function() {
+  it('should not be the same twice', () => random().should.not.equal(random()));
+  return it('should be 16 digits', () => random().length.should.equal(16));
+}));
